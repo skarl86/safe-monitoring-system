c77b9ca612fdebd485024b25cd8d20301d4615e6
skarl86 <beohemian@gmail.com>
2015-03-03 19:57:21 +0900

    1, 형태소분석기 모듈 수정.

--- src/module/scala/MorphoAnalysis.scala
+++ src/module/scala/MorphoAnalysis.scala
@@ -1,7 +1,10 @@
 import java.io.{File, PrintWriter}
 
-import org.chasen.mecab.Tagger
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
+import scala.collection.mutable._
 import scala.io.Source._
 import scala.collection.JavaConverters._
 
@@ -10,10 +13,10 @@ import scala.collection.JavaConverters._
  */
 class MorphoAnalysis {
   System.load(System.getProperty("java.library.path") + "/libMeCab.so")
-
-  private val mecab = new MeCab()
-
-  private val tagger = new Tagger("-d /usr/local/lib/mecab/dic/mecab-ko-dic")
+  private val _wordClass: String = "(.*NNG.*|.*NNP.*|.*NNB.*|.*NR.*|.*NP.*|.*SL.*)"
+  private val _mecab = new MeCab()
+  private val _conf = new SparkConf().setAppName("makeKeyword").setMaster("local[*]")
+  private val _sc = new SparkContext(_conf)
 
   def makeKeywordInTweet(inputPath :String, outputPath: String): Unit ={
     val source = fromFile(inputPath)
@@ -21,10 +24,41 @@ class MorphoAnalysis {
     val writer = new PrintWriter(new File(outputPath))
 
     for(line <- lines){
-      mecab.parseWord(line).asScala.foreach(word => writer.write(word + "\t"))
+      _mecab.parseWord(line).asScala.foreach(word => writer.write(word + ","))
       writer.write("\n")
     }
     writer.close()
     source.close()
+  }
+  def makeRDDKeywordInTweet(inputPath :String): Unit ={
+
+
+    val tweet = _sc.textFile(inputPath).map(_.split("\t")(2))
+//    tweet.collect().foreach(println)
+    println(parseWord(tweet))
+  }
+
+  def parseTweet(rowData:String): String ={ rowData.split("\t")(2) }
+
+  def parseWord(tweetRDD: RDD[String]): Unit ={
+    val words = MutableList[String]()
+
+    for(tweet <- tweetRDD.collect()){
+      words += _mecab.parseWord(tweet).asScala.mkString(",")
+    }
+    filterStopWord(words)
+  }
+
+  def filterStopWord(words: MutableList[String]): Unit ={
+    val stopword = fromFile("./dic/stopword").getLines().toString().split(",")
+    for(word <- words){
+//      if(stopword.contains(word))
+////        words
+    }
+  }
+
+  def isStopWord(word: String): Boolean = {
+    val stopwordsBrod = fromFile("./dic/stopword").getLines().toString().split(",")
+    stopwordsBrod.contains(word)
   }
 }
