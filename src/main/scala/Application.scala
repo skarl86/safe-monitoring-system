b94d4f76c09f544ebc2c29c288b262425b23be88
JasonHong <beggar3004@gmail.com>
2015-03-03 20:22:03 +0900

    Application 수정 - SparkConf, SparkContext

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,12 +1,22 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * Created by NCri on 15. 3. 3..
  */
 object Application {
   def main(args: Array[String]) {
-    val morpho = new MorphoAnalysis
-    val tfidf = new TFIDF
 
-    morpho.makeKeywordInTweet("Input path", "Output path")
-    tfidf.run
+    val conf = new SparkConf().setAppName("Safe Monitoring System").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    val morpho = new MorphoAnalysis
+//    val featureExtractor = new FeatureExtractor(sc)
+
+//    morpho.makeKeywordInTweet("input/data.txt", "input/keyword.txt")
+
+//    val corpus = sc.textFile("input/keyword.txt").map(_.split(",").toList).map(_.tail.toSeq)
+
+//    val keywordTfidf = featureExtractor.tfidf(corpus, )
+
   }
 }
