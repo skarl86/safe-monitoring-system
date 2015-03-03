0be99b6561a27a32176285647f4ffb77c589bd0f
JasonHong <beggar3004@gmail.com>
2015-03-03 22:37:01 +0900

    - TFIDF => FeatureExtractor -- Entropy 계산 함수 추가

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.rdd.RDD
+
 
 /**
  * Created by NCri on 15. 3. 3..
@@ -9,14 +11,23 @@ object Application {
     val conf = new SparkConf().setAppName("Safe Monitoring System").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val morpho = new MorphoAnalysis
-//    val featureExtractor = new FeatureExtractor(sc)
+    //val morpho = new MorphoAnalysis
+    val featureExtractor = new FeatureExtractor(sc)
 
 //    morpho.makeKeywordInTweet("input/data.txt", "input/keyword.txt")
 
-//    val corpus = sc.textFile("input/keyword.txt").map(_.split(",").toList).map(_.tail.toSeq)
+    val corpus: RDD[Seq[String]] = sc.textFile("input/data.txt").map(_.split("\t")).map(list => list(1).split(" ").toSeq)
+//    val corpus: RDD[(String, String)] = sc.textFile("input/data.txt").map(_.split("\t")).map(list => (list(0), list(1)))
+    val keywords: RDD[String] = sc.textFile("input/keyword.txt").map(_.split(",").toSeq).flatMap(_.tail).distinct
 
-//    val keywordTfidf = featureExtractor.tfidf(corpus, )
+//    corpus.foreach(println)
 
+    val keywordTfidf = featureExtractor.tfidf(corpus)
+    keywordTfidf.coalesce(1).saveAsTextFile("output")
+
+//    corpus.filter(_._2.contains("설정")).foreach(println)
+
+//    val entropy = featureExtractor.entropy(corpus, keywords)
+//    entropy.saveAsTextFile("output")
   }
 }
