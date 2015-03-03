4f2660fe158f3bb2c471cd888e883bccb567e838
JasonHong <beggar3004@gmail.com>
2015-03-03 15:23:34 +0900

    1. TFIDF 수정 - SparkContext._ import 수정

--- src/module/scala/TFIDF.scala
+++ src/module/scala/TFIDF.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.feature.IDF
 import org.apache.spark.mllib.linalg.Vector
