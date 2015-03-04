157b4159fa8b74d3270803cb6fe810492198107b
skarl86 <beohemian@gmail.com>
2015-03-04 19:52:16 +0900

    1. 로컬 테스트 환경 구성.

--- src/module/java/MeCab.java
+++ src/module/java/MeCab.java
@@ -48,10 +48,12 @@ public class MeCab {
             if(!line.equals("EOS")){
                 key = line.split("\t")[0];
                 feature = line.split("\t")[1].split(",")[0];
-                if(feature.matches(_wordClass)){
-                    // 불용어 처리.
-                    if(!_stopwordList.contains(key))
-                        wordList.add(key);
+                if(key.length() > 1){
+                    if(feature.matches(_wordClass)){
+                        // 불용어 처리.
+                        if(!_stopwordList.contains(key))
+                            wordList.add(key);
+                    }
                 }
             }
         }
@@ -60,37 +62,54 @@ public class MeCab {
     }
     List<String> getStopWord(){
         ArrayList<String> words = new ArrayList<String >();
-        try
-        {
-            // create our mysql database connection
-            String myDriver = "org.gjt.mm.mysql.Driver";
-            String myUrl = "jdbc:mysql://218.54.47.24:3306/tweet";
-            Class.forName(myDriver);
-            Connection conn = DriverManager.getConnection(myUrl, "root", "tkfkdgo1_");
-
-            // our SQL SELECT query.
-            // if you only need a few columns, specify them by name instead of using "*"
-            String query = "SELECT stopword FROM stopwordtable";
-
-            // create the java statement
-            Statement st = conn.createStatement();
-
-            // execute the query, and get a java resultset
-            ResultSet rs = st.executeQuery(query);
-
-            // iterate through the java resultset
-            while (rs.next())
-            {
-                String stopword = rs.getString("stopword");
-                words.add(stopword);
+        BufferedReader br = null;
+        String line = null;
+        try{
+            br = new BufferedReader(new FileReader("./dic/stopword.txt"));
+            line = br.readLine();
+            for(String word : line.split(",")){
+                words.add(word);
             }
-            st.close();
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
-            System.err.println("Got an exception! ");
-            System.err.println(e.getMessage());
-        }
+
         return words;
+
+
+
+//        try
+//        {
+//            // create our mysql database connection
+//            String myDriver = "org.gjt.mm.mysql.Driver";
+//            String myUrl = "jdbc:mysql://218.54.47.24:3306/tweet";
+//            Class.forName(myDriver);
+//            Connection conn = DriverManager.getConnection(myUrl, "root", "tkfkdgo1_");
+//
+//            // our SQL SELECT query.
+//            // if you only need a few columns, specify them by name instead of using "*"
+//            String query = "SELECT stopword FROM stopwordtable";
+//
+//            // create the java statement
+//            Statement st = conn.createStatement();
+//
+//            // execute the query, and get a java resultset
+//            ResultSet rs = st.executeQuery(query);
+//
+//            // iterate through the java resultset
+//            while (rs.next())
+//            {
+//                String stopword = rs.getString("stopword");
+//                words.add(stopword);
+//            }
+//            st.close();
+//        }
+//        catch (Exception e)
+//        {
+//            System.err.println("Got an exception! ");
+//            System.err.println(e.getMessage());
+//        }
+//        return words;
     }
 }
